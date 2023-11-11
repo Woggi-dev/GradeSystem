b02e9b79b09f65a23185632e2a38cc3d4ad6866f
Студент <@DESKTOP-98S7406>
2023-11-11 13:24:20 +0300

    test commit

--- forms/Form1.cs
+++ forms/Form1.cs
@@ -11,7 +11,8 @@ using System.Windows.Forms;
 namespace GradeSystem
 {
     public partial class Form1 : Form
-    {
+    
+        //test
         public Form1()
         {
             InitializeComponent();
