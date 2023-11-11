83c01e4904ba998c482a694378e66dac31f0ccaa
W0ggi <voron529403@mail.ru>
2023-11-11 22:13:28 +0300

    bug fix

--- forms/Form1.cs
+++ forms/Form1.cs
@@ -11,8 +11,7 @@ using System.Windows.Forms;
 namespace GradeSystem
 {
     public partial class Form1 : Form
-    
-        //test///
+    {
         public Form1()
         {
             InitializeComponent();
