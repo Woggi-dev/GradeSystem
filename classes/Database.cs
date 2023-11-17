6d16a75e68a6e15a2bfa466d313f99b622b74c6b
Agissimon <caramel3355for@gmail.com>
2023-11-17 12:29:12 +0300

    created db connection

--- classes/Database.cs
+++ classes/Database.cs
@@ -9,47 +9,29 @@ namespace GradeSystem.scripts
 {
     class Database
     {
-        static void Connect()
+        public static void Connect()
         {
             // Строка подключения
-            string connectionString = "Data Source=your_server;Initial Catalog=your_database;User ID=your_username;Password=your_password";
+            string sqlConnection = @"Data Source=DESKTOP-O4H69NN\SQLEXPRESS;Initial Catalog=Grade_System;Integrated Security=True";
 
             // Создание объекта SqlConnection
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(sqlConnection))
             {
                 try
                 {
                     // Открытие подключения
                     connection.Open();
                     Console.WriteLine("Подключение открыто");
+                    connection.Close();
+                    Console.WriteLine("Подключение закрыто");
 
-                    // Здесь вы можете выполнять операции с базой данных
-
-                    // Пример выполнения SQL-запроса
-                    string sqlQuery = "SELECT * FROM YourTable";
-                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
-                    {
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                // Обработка данных из результата запроса
-                                Console.WriteLine(reader["ColumnName"]);
-                            }
-                        }
-                    }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine($"Ошибка: {ex.Message}");
                 }
-                finally
-                {
-                    // Закрытие подключения в блоке finally для обеспечения корректного закрытия даже в случае исключения
-                    connection.Close();
-                    Console.WriteLine("Подключение закрыто");
-                }
             }
         }
+        
     }
 }
